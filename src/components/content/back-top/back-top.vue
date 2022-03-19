4bc69625681b8ea047ae075d7438efa04f916b68
gin <215129113@qq.co>
2022-03-19 23:10:05 +0800

    feat(components/content/back-top): 封装backtop组件

---
+++ src/components/content/back-top/back-top.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="back-top">
+    <img src="~@/assets/images/home/top.png" alt="" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.back-top {
+  position: fixed;
+  z-index: 997;
+  bottom: 2rem;
+  right: 0.26rem;
+
+  img {
+    width: 1rem;
+  }
+}
+</style>
